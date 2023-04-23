fdc45e39801ff695a968e225d83d8eb8a0f08e55
Niclas D. Gesing <nicdomgesing@gmail.com>
2023-04-23 22:14:06 +0000

    :sparkles: adds several plugins

--- plugins/community.lua
+++ plugins/community.lua
@@ -5,5 +5,10 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.motion.leap-nvim" },
+  { import = "astrocommunity.test.neotest" },
+  -- { import = "astrocommunity.comment.mini-comment" },
+  { import = "astrocommunity.editing-support.todo-comments-nvim" },
+  { import = "astrocommunity.pack.python" }
 }
