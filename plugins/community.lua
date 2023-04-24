f8b356b1d0014c1b0dd5d5f300987227e3f00d1f
Niclas D. Gesing <nicdomgesing@gmail.com>
2023-04-24 21:04:39 +0000

    :sparkles: adds project-nvim

--- plugins/community.lua
+++ plugins/community.lua
@@ -10,5 +10,5 @@ return {
   { import = "astrocommunity.test.neotest" },
   { import = "astrocommunity.editing-support.todo-comments-nvim" },
   { import = "astrocommunity.pack.python" },
-  -- { import = "astrocommunity.project.project-nvim" }
+  { import = "astrocommunity.project.project-nvim" }
 }
