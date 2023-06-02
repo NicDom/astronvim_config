a6bea91401e6777265622d071f728a89dfba110d
Niclas D. Gesing <nicdomgesing@gmail.com>
2023-06-02 07:59:20 +0000

    :alembic: removes pylint

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -14,7 +14,7 @@ return {
     -- overrides `require("mason-null-ls").setup(...)`
     opts = {
       -- ensure_installed = { "prettier", "stylua" },
-      ensure_installed = { "pyupgrade", "pydocstyle" },
+      ensure_installed = { "black", "isort" },
     },
   },
   {
